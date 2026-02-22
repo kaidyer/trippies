7fcbe6ca7cedf773bcf758992fd186d348f74cc4
kaidyer <kaidyer29@gmail.com>
2026-02-21 19:29:10 -0500

    Initial Commit

---
+++ Trippies/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Trippies
+//
+//  Created by Kai Dyer on 2/21/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Trippies/TrippiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrippiesApp.swift
+//  Trippies
+//
+//  Created by Kai Dyer on 2/21/26.
+//
+
+import SwiftUI
+
+@main
+struct TrippiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
